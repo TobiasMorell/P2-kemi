807918065296a517df17ae1b574e10db548d5958
Jacob <holm.mortensen@hotmail.com>
2015-02-20 11:13:57 +0100

    Simpel graf

---
+++ WindowsFormsApplication3/WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Start1 = new System.Windows.Forms.Button();
+            this.Start2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Start1
+            // 
+            this.Start1.Location = new System.Drawing.Point(527, 12);
+            this.Start1.Name = "Start1";
+            this.Start1.Size = new System.Drawing.Size(75, 23);
+            this.Start1.TabIndex = 0;
+            this.Start1.Text = "Start 1";
+            this.Start1.UseVisualStyleBackColor = true;
+            this.Start1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Start2
+            // 
+            this.Start2.Location = new System.Drawing.Point(527, 41);
+            this.Start2.Name = "Start2";
+            this.Start2.Size = new System.Drawing.Size(75, 23);
+            this.Start2.TabIndex = 1;
+            this.Start2.Text = "Start 2";
+            this.Start2.UseVisualStyleBackColor = true;
+            this.Start2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 521);
+            this.Controls.Add(this.Start2);
+            this.Controls.Add(this.Start1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start1;
+        private System.Windows.Forms.Button Start2;
+    }
+}
+
---
+++ WindowsFormsApplication3/WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Paint += Axis;
+        }
+
+        private void Axis(object sender, PaintEventArgs e)
+        {
+            Point[] graph = { new Point(10, 10),    new Point(10, 510),     new Point(510, 510),
+                              new Point(2, 18),     new Point(10, 11),      new Point(17, 18),
+                              new Point(502, 502),  new Point(511, 510),    new Point(502, 517)};
+            Graphics g = e.Graphics;
+            g.DrawLine(new Pen(Brushes.Black, 4), graph[0], graph[1]);
+            g.DrawLine(new Pen(Brushes.Black, 4), graph[1], graph[2]);
+            g.DrawLine(new Pen(Brushes.Black, 3), graph[3], graph[4]);
+            g.DrawLine(new Pen(Brushes.Black, 3), graph[4], graph[5]);
+            g.DrawLine(new Pen(Brushes.Black, 3), graph[6], graph[7]);
+            g.DrawLine(new Pen(Brushes.Black, 3), graph[7], graph[8]);
+            this.BackColor = Color.Gray;
+        }
+
+        Thread t1, t2;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            t1 = new Thread(thread1);
+            t1.Start(); 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            t2 = new Thread(thread2);
+            t2.Start();
+        }
+
+        public void thread1()
+        {
+            Random rand = new Random();
+            int x = 10, y = 100;
+            for (int i = 10; i < 510; i += 6)
+            {
+                this.CreateGraphics().DrawLine(new Pen(Brushes.White, 1), new Point(x, y), new Point((x = i), (y = 100 + (int)(Math.Sin(i) * 100))));
+                System.Threading.Thread.Sleep(20);
+            }
+        }
+
+        public void thread2()
+        {
+            Random rand = new Random();
+            int x = 10, y = 400;
+            for (int i = 10; i < 510; i += 3)
+            {
+                this.CreateGraphics().DrawLine(new Pen(Brushes.White, 1), new Point(x, y), new Point((x = i), (y = 400 + (int)(Math.Sin(i) * 100))));
+                System.Threading.Thread.Sleep(40);
+            }
+        }
+    }
+}
