b79a065f49ce34e3cdcebc295405f1ac8d3e7f1d
Arne Rasmussen <Gedesneglen@gmail.com>
2015-02-23 12:51:04 +0100

    Added ned class, Graph

---
+++ WindowsFormsApplication3/WindowsFormsApplication3/Graph.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace WindowsFormsApplication3
+{
+    class Graph : Panel
+    {
+        Graphics g;
+
+        public Graph(int x, int y, int h, int w)
+        {
+            this.Height = h;
+            this.Width = w;
+            this.Left = x;
+            this.Top = y;
+
+            Graphics g = this.CreateGraphics();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+ 	        
+        }
+    }
+}
